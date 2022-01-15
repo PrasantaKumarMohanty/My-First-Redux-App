d27a4bcd495507cf69af6cc1c342de716029431a
Prasanta Kumar Mohanty <mprasantakumar72@gmail.com>
2022-01-15 10:49:10 +0530

    sec push

--- react-redux-app/src/MyComponent/Reducers/RootReducers.js
+++
@@ -1,8 +0,0 @@
-import BookReducers from "./BookReducers";
-import { combineReducers } from "redux";
-
-const RootReducers = combineReducers({
-    BookReducers:BookReducers
-});
-
-export default RootReducers;
--- react-redux-app/src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import RootReducers from "./MyComponent/Reducers/RootReducers";
-
-const store=createStore(RootReducers);
-
-export default store;
